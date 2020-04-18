2ab64c7d1a7913dbe53532906302b9c1c930e976
AbhilashUppuluri <abhilashuppuluri.7@gmail.com>
2020-04-19 01:10:59 +0530

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LazyDataTableComponent } from './lazy-data-table/lazy-data-table.component';
+import { DataTableComponent } from './data-table/data-table.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'data-table', component: DataTableComponent, pathMatch: 'full' },
+  { path: 'lazy-data-table', component: LazyDataTableComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'data-table', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
